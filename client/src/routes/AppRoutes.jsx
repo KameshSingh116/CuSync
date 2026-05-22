54e0e62f3bc4709d8bdada187ca87294a836a2b2
Kamesh Singh <radhakrishnapg152627@gmail.com>
2026-05-23 00:28:18 +0530

    routes

---
+++ client/src/routes/AppRoutes.jsx
@@ -0,0 +1,30 @@
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
+
+import Login from "../pages/auth/Login";
+import Signup from "../pages/auth/Signup";
+
+function AppRoutes() {
+  return (
+    <BrowserRouter>
+      <Routes>
+
+        <Route
+          path="/login"
+          element={<Login />}
+        />
+
+        <Route
+          path="/signup"
+          element={<Signup />}
+        />
+
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default AppRoutes;
