c9df16f1c45d5c1f00e7408654501bbec2475eec
Kamesh Singh <radhakrishnapg152627@gmail.com>
2026-05-23 20:58:43 +0530

    made the dashboard base

---
+++ client/src/pages/dashboard/Dashboard.jsx
@@ -0,0 +1,13 @@
+function Dashboard() {
+  return (
+    <div className="min-h-screen bg-slate-900 text-white p-10">
+
+      <h1 className="text-4xl font-bold">
+        Welcome to CuSync Dashboard
+      </h1>
+
+    </div>
+  );
+}
+
+export default Dashboard;
